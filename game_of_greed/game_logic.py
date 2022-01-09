f4a870cff9d2eaa11c7763fae19992e9c1936d4a
Ediberto Ponce <ediberto01@gmail.com>
2022-01-08 18:05:54 -0800

    utilized rogers code game_logic in hope of fixing game.py

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,95 +1,138 @@
 import random
 from collections import Counter
 
-
+# Used Roger's Game_Logic Code for this module
 class GameLogic:
 
-    @staticmethod
-    def roll_dice(dice_roll):
-        dice_list = []
-        for _ in range(dice_roll):
-            dice_list.append(random.randint(1, 6))
-        return tuple(dice_list)
+  @staticmethod
+  def calculate_stuff(tuple_of_dice, type_of_return):
 
-    @staticmethod
-    def calculate_score(dice):
-        score = 0
-        count = Counter(dice[:6])
-        straight = sorted(dice)
+    score = 0
+    scoring_dice = []
+    counts = Counter(tuple_of_dice)
 
-        if count[5] == 1 or count[5] == 2:
-            score += + 50 * count[5]
+    if tuple_of_dice and set(tuple_of_dice) == set((1, 2, 3, 4, 5, 6)):
+        score += 1500
+        scoring_dice.extend([*tuple_of_dice])
+    elif len(counts) == 3 and set(counts.values()) == set((2,)):
+        score += 1500
+        scoring_dice.extend([*tuple_of_dice])
+    else:
+        # Counter([1, 1, 1, 1, 2]) --> {key: count, key: count} --> counts = {1: 4, 2: 1}
+        # {1: 4, 2: 1} --> key = 1, count = 4 --> key = 2, count = 1
+        for key, count in counts.items():
+            if count >= 3:
+                if key == 1:
+                    score += (count - 2) * 1000
+                    scoring_dice.extend([key for _ in range(count)])
+                else:
+                    score += (count - 2) * key * 100
+                    scoring_dice.extend([key for _ in range(count)])
+            else:
+                if key == 5:
+                    score += 50 * count
+                    scoring_dice.extend([key for _ in range(count)])
+                if key == 1:
+                    score += 100 * count
+                    scoring_dice.extend([key for _ in range(count)])
 
-        if count[1] == 1 or count[1] == 2:
-            score += + 100 * count[1]
-
-        # Handles Straight Die
-        if straight == [1, 2, 3, 4, 5, 6]:
-            score = 1500
-            return score
-
-        # Handles three of a kind
-        for i in range(1, 7):
-            if i == 1 and count[1] == 3:
-                score += 1000
-            elif i != 1 and count[i] == 3:
-                score += i * 100
-
-        # Handles four of a kind
-        for i in range(1, 7):
-            if i == 1 and count[1] == 4:
-                score += 2000
-            elif i != 1 and count[i] == 4:
-              score += i * 100 * 2
-
-        # Handles five of a kind
-        for i in range(1, 7):
-            if i == 1 and count[1] == 5:
-                score += 3000
-            elif i != 1 and count[i] == 5:
-              score += i * 100 * 3 
-              
-        # Handles six of a kind
-        for i in range(1, 7):
-            if i == 1 and count[1] == 6:
-                score += 4000
-            elif i != 1 and count[i] == 6:
-              score += i * 100 * 4           
-
+    if type_of_return == 'score':
         return score
+    elif type_of_return == 'scoring_dice':
+        return tuple(scoring_dice)
 
-    @staticmethod
-    def validate_keepers(roll, keepers):
-        # version_3
-        # pro tip: you can do some math operations with counters
-        # check https://docs.python.org/3/library/collections.html#collections.Counter
-        keeper_count = Counter(keepers)
-        roll_count = Counter(roll)
+  @staticmethod
+  def calculate_score(tuple_of_dice):
+        return GameLogic.calculate_stuff(tuple_of_dice, 'score')
 
-        # a "valid" result is an empty Counter result
-        game_result = keeper_count - roll_count
+  @staticmethod
+  def get_scorers(tuple_of_dice):
+        return GameLogic.calculate_stuff(tuple_of_dice, 'scoring_dice')
 
-        # an empty Counter is falsy, so use "not" to flip it
-        return not game_result
+  @staticmethod
+  def roll_dice(amount_of_dice_1_to_6):
 
-    @staticmethod
-    def get_scorers(dice):
-        # version_3
+    temporary_list = []
 
-        all_dice_score = GameLogic.calculate_score(dice)
+    for _ in range(amount_of_dice_1_to_6):
+        temporary_list.append(random.randint(1, 6))
 
-        if all_dice_score == 0:
-            return tuple()
+    return tuple(temporary_list)
 
-        scorers = []
+  @staticmethod
+  def validate_keepers(tuple_of_rolled_dice,tuple_of_keepers_possible):
+    actual = sorted(list(GameLogic.get_scorers(tuple_of_rolled_dice)))
+    expected = sorted(list(tuple_of_keepers_possible))
 
-        # for i in range(len(dice)):
+    if actual == expected:
+      return True
+    else:
+      return False
 
-        for i, val in enumerate(dice):
-            sub_roll = dice[:i] + dice[i + 1 :]
-            sub_score = GameLogic.calculate_score(sub_roll)
+  @staticmethod
+  def validate_input(tuple_of_rolled_dice, user_input):
+    counter_rolled = Counter(tuple_of_rolled_dice)
+    counter_input = Counter(user_input)
 
-            if sub_score != all_dice_score:
-                scorers.append(val)
+    for key, user_count in counter_input.items():
+        if user_count > counter_rolled[key]:
+            return False
+    
+    return True
 
-        return tuple(scorers)
+
+# class GameLogic:
+
+#     @staticmethod
+#     def roll_dice(dice_roll):
+#         dice_list = []
+#         for _ in range(dice_roll):
+#             dice_list.append(random.randint(1, 6))
+#         return tuple(dice_list)
+
+#     @staticmethod
+#     def calculate_score(dice):
+#         score = 0
+#         count = Counter(dice[:6])
+#         straight = sorted(dice)
+
+#         if count[5] == 1 or count[5] == 2:
+#             score += + 50 * count[5]
+
+#         if count[1] == 1 or count[1] == 2:
+#             score += + 100 * count[1]
+
+#         # Handles Straight Die
+#         if straight == [1, 2, 3, 4, 5, 6]:
+#             score = 1500
+#             return score
+
+#         # Handles three of a kind
+#         for i in range(1, 7):
+#             if i == 1 and count[1] == 3:
+#                 score += 1000
+#             elif i != 1 and count[i] == 3:
+#                 score += i * 100
+
+#         # Handles four of a kind
+#         for i in range(1, 7):
+#             if i == 1 and count[1] == 4:
+#                 score += 2000
+#             elif i != 1 and count[i] == 4:
+#               score += i * 100 * 2
+
+#         # Handles five of a kind
+#         for i in range(1, 7):
+#             if i == 1 and count[1] == 5:
+#                 score += 3000
+#             elif i != 1 and count[i] == 5:
+#               score += i * 100 * 3 
+              
+#         # Handles six of a kind
+#         for i in range(1, 7):
+#             if i == 1 and count[1] == 6:
+#                 score += 4000
+#             elif i != 1 and count[i] == 6:
+#               score += i * 100 * 4           
+
+#         return score
